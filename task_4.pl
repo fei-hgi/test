f4a10dc21767c1f2f067d9e62fc4e501fbf4e05c
fs18 <fs18@sanger.ac.uk>
2023-02-15 15:54:48 +0000

    modified:   task_4.pl add sequence counting

--- task_4.pl
+++ task_4.pl
@@ -27,4 +27,23 @@ else
     open IN, "<$fqFile" or die "Cannot open the fastq file to read.\n";
 }
 
+my $seqnum = 0;
+
+if($opt_s)
+{
+    print "====> Couting the number of sequences.\n";
+    while(<IN>)
+    {
+        chomp $_;
+        if($_ ne "")
+        {
+            $seqnum++;
+        }
+    }
+    print "No. of sequences: ".($seqnum/4)."\n";
+}
+else
+{
+    print "====> Nothing Running, please provide flag to count.\n"
+}
 
